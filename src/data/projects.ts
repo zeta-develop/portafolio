5753ea78b5455425a49f2177c3f57176c36f4aa7
Ronald Tellez <80638821+zeta-develop@users.noreply.github.com>
2026-03-13 20:16:44 -0600

    Change project image format from PNG to WEBP

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -15,7 +15,7 @@ const projects: Project[] = [
     id: 1,
     name: "Eshop - Admin",
     description: "Eshop es un SaaS de comercio electronico e inventario multi-tenant diseñada para permitir que multiples negocios gestionen sus tiendas desde un sistema centralizado mientras sus datos se encuentran totalmente aislados",
-    image: "/proyects/eshop.png",
+    image: "/proyects/eshop.webp",
     topics: ["Nextjs", "SupaBase", "api"],
     featured: true,
     demo: "https://eshop.zetastudio.space",
