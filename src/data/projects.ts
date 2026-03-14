6a9b0b320d24a805175fcda02439d4a47fe511e1
Ronald Tellez <80638821+zeta-develop@users.noreply.github.com>
2026-03-13 20:15:47 -0600

    Modify Eshop project details
    Updated image path and demo URL for Eshop project.

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -15,10 +15,10 @@ const projects: Project[] = [
     id: 1,
     name: "Eshop - Admin",
     description: "Eshop es un SaaS de comercio electronico e inventario multi-tenant diseñada para permitir que multiples negocios gestionen sus tiendas desde un sistema centralizado mientras sus datos se encuentran totalmente aislados",
-    image: "/eshop.png",
+    image: "/proyects/eshop.png",
     topics: ["Nextjs", "SupaBase", "api"],
     featured: true,
-    demo: "https://nexusbill.ronaldtellez.online",
+    demo: "https://eshop.zetastudio.space",
   },
 ];
 
