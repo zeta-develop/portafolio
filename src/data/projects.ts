aa2fc0c565a9a04dcaed179cd2894db55d709d2b
Ronald Tellez <80638821+zeta-develop@users.noreply.github.com>
2026-03-13 20:10:23 -0600

    Update projects list with new Eshop project
    Removed multiple project entries and added a new project 'Eshop - Admin' with its details.

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -13,29 +13,11 @@ export interface Project {
 const projects: Project[] = [
   {
     id: 1,
-    name: "NexusCP",
-    description: "Panel de control para la administración de servicios de hosting, con soporte para múltiples servidores y usuarios.",
-    image: "/projects/NexusCP.webp",
-    topics: ["fullstack", "react", "nextjs", "PrismaORM", "TypeScript"],
-    demo: "https://nexuscp.ronaldtellez.online",
-    featured: true
-  },
-  {
-    id: 2,
-    name: "PterodactylCP",
-    description: "Cliente web para la administración de servidores de juegos, utilizando Pterodactyl Panel.",
-    image: "/projects/pterodactylcp.webp",
-    topics: ["Nextjs", "API", "LocalStorage"],
-    github: "https://github.com/zeta-develop/PterodactylCP",
-    demo: "https://ptcp.ronaldtellez.online",
-    featured: true
-  },
-  {
-    id: 3,
-    name: "Nexus Billing",
-    description: "Sistema de facturación y gestión de clientes para un proveedor de servicios de hosting.",
-    image: "https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?w=500&h=300&auto=format&fit=crop&q=80",
-    topics: ["Laravel", "Nextjs", "postgreSQL"],
+    name: "Eshop - Admin",
+    description: "Eshop es un SaaS de comercio electronico e inventario multi-tenant diseñada para permitir que multiples negocios gestionen sus tiendas desde un sistema centralizado mientras sus datos se encuentran totalmente aislados",
+    image: "/eshop.png",
+    topics: ["Nextjs", "SupaBase", "api"],
+    featured: true,
     demo: "https://nexusbill.ronaldtellez.online",
   },
 ];
